f69fa47a3e42df07c70a81a9c829773473ed9e38
JarekSour <numetalsour@gmail.com>
2025-02-26 14:55:44 -0300

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import "zurich-cl-sdk/dist/slider";
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'angular-example-sdk-cl';
+
+  onChangeSlider(evt: Event) {
+    if (evt.target) {
+      console.log(evt, (evt.target as HTMLInputElement).value, (evt as CustomEvent).detail.value);
+    }
+  }
 }
